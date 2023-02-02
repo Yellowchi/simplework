4ce849100e389d47e5ac8544d8ff1b450a05a0ce
丛杨 <1370065936@qq.com>
2023-02-02 15:34:47 +0800

    Default Changelist

--- src/test/java/com/simplework/simplework/SimpleWorkApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.simplework.simplework;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SimpleWorkApplicationTests {
-    public SimpleWorkApplicationTests() {
-    }
-}
