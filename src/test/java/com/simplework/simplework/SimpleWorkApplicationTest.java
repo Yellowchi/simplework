c05602b4da95fa7473f3aad276cfb3c85bf452d9
丛杨 <1370065936@qq.com>
2023-02-03 10:51:01 +0800

    Default Changelist

---
+++ src/test/java/com/simplework/simplework/SimpleWorkApplicationTest.java
@@ -0,0 +1,12 @@
+package com.simplework.simplework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class SimpleWorkApplicationTest {
+
+    @Test
+    void main() {
+        System.out.println("Test");
+    }
+}
